fc0802102dd39859f56c3f3b0a26a85e0bcca127
mitskevich-paul <58680035+mitskevich-paul@users.noreply.github.com>
2020-04-01 21:19:37 +0300

    Update js.js

--- js.js
+++ js.js
@@ -6,30 +6,30 @@ start.addEventListener('click',() => {
     c.height = 720;
     document.body.appendChild(c);
     
-    let perm = [];
-    while (perm.length < 255) {
-        while (perm.includes(val = Math.floor(Math.random()*255)));
-        perm.push(val);
+    let ground = [];
+    while (ground.length < 255) {
+        while (ground.includes(val = Math.floor(Math.random()*255)));
+        ground.push(val);
     }
     
-    let lerp = (a, b, t) => 2 * a + (2 * b - 2 * a) * (1 - Math.cos(t * Math.PI)) / 2;
-    let noise = x => {
+    let wavesHight = (a, b, t) => 2 * a + (2 * b - 2 * a) * (1 - Math.cos(t * Math.PI)) / 2;
+    let waves = x => {
         x = x * 0.005 % 255;
-        return lerp(perm[Math.floor(x)], perm[Math.ceil(x)], x - Math.floor(x));
+        return wavesHight(ground[Math.floor(x)], ground[Math.ceil(x)], x - Math.floor(x));
     }
     
     let player = new function() {
         this.x = c.width / 3;
-        this.y = 0;
+        this.y = c.height / 2;
         this.ySpeed = 0;
-        this.rot = 0;
+        this.rotated = 0;
         this.rSpeed = 0;
     
         this.img = new Image();
         this.img.src = './1.png';
         this.draw = function() {
-            let p1 = c.height - noise(t + this.x) * 0.55; 
-            let p2 = c.height - noise(t + 3 + this.x) * 0.55; 
+            let p1 = c.height - waves(acceleration + this.x) * 0.55; 
+            let p2 = c.height - waves(acceleration + 3 + this.x) * 0.55; 
             let grounded = 0;
     
             if (p1 - 15 > this.y) {
@@ -39,8 +39,7 @@ start.addEventListener('click',() => {
                 this.y = p1 - 15;
                 grounded = 1;
             }
-    
-            if (!playing || grounded && Math.abs(this.rot) > Math.PI * 0.5) {
+            if (!playing || grounded && Math.abs(this.rotated) > Math.PI * 0.5) {
                 playing = false;
                 this.rSpeed = 5;
                 k.ArrowUp = 1;
@@ -55,34 +54,38 @@ start.addEventListener('click',() => {
                 document.querySelector('input').value = score;
             }
             if (grounded && playing) {
-                this.rot -= (this.rot - angle) * 0.5;
-                this.rSpeed = this.rSpeed - (angle - this.rot);
+                this.rotated -= (this.rotated - angle) * 0.5;
+                this.rSpeed = this.rSpeed - (angle - this.rotated);
             }
             this.rSpeed += (k.ArrowLeft - k.ArrowRight) * 0.05;
-            this.rot -= this.rSpeed * 0.1;
+            this.rotated -= this.rSpeed * 0.1;
     
-            if (this.rot > Math.PI) {
-                this.rot = -Math.PI;
-            } else if (this.rot < -Math.PI) {
-                this.rot = Math.PI;
+            if (this.rotated > Math.PI) {
+                this.rotated = -Math.PI;
+            } else if (this.rotated < -Math.PI) {
+                this.rotated = Math.PI;
             }
     
             ctx.save();
             ctx.translate(this.x, this.y);
-            ctx.rotate(this.rot)
+            ctx.rotate(this.rotated)
             ctx.drawImage(this.img, -70, -70, 100, 100);
             ctx.restore();
         }
     }
     
     let score = 0;
-    let t = 0;
+    let acceleration = 0;
     let speed = 0;
     let playing = true;
     let k = {ArrowUp : 0, ArrowDown : 0, ArrowLeft : 0, ArrowRight : 0};
     function loop() {
-        speed -= (speed - (k.ArrowUp - k.ArrowDown)) * 0.01;
-        t += 10 * speed;
+        if (speed >= 0) {
+            speed -= (speed - (k.ArrowUp - k.ArrowDown)) * 0.01;
+            acceleration += 10 * speed;
+        } else if (speed < 0) {
+            speed = 0
+        }
         ctx.fillStyle = 'rgb(80, 200, 255)';
         ctx.fillRect(0, 0, c.width, c.height);
     
@@ -90,7 +93,7 @@ start.addEventListener('click',() => {
         ctx.beginPath();
         ctx.moveTo(0, c.height);
         for (let i = 0; i < c.width; i++) {
-            ctx.lineTo (i, c.height - noise(t + i) * 0.55);
+            ctx.lineTo (i, c.height - waves(acceleration + i) * 0.55);
         }
     
         ctx.lineTo(c.width, c.height);
